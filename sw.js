2d475ef8eaea1e5b86cb5482fa7e56de8ccbda61
azkargorta <azkardurant@gmail.com>
2026-09-02 12:50:42 +0200

    Refresh PWA cache for approved logo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'basketball-gm-beta-v021-mobile2';
+const CACHE_NAME = 'basketball-gm-beta-v021-logo1';
 const CORE = [
   './',
   './index.html',
@@ -9,9 +9,7 @@ const CORE = [
   './js/app.js',
   './js/pwa.js',
   './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-180.png'
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {

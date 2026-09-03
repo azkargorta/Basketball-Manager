9804c0646c6769fe9e3a159e28b750573b6b0a9d
azkargorta <azkardurant@gmail.com>
2026-09-03 16:38:38 +0200

    Add SC Derby and Borac 2026-27 rosters

---
+++ js/real-rosters-manual-batch11-2026-27.js
@@ -0,0 +1,11 @@
+(function(g){'use strict';
+const B={
+93:[
+{name:'Andre Wesson',position:'SF',age:29,ovr:76,potential:76},{name:'Jyare Davis',position:'SF',age:24,ovr:75,potential:78},{name:'Aleksa Uskokovic',position:'PG',age:27,ovr:75,potential:77},{name:'Mateo Dreznjak',position:'SG',age:27,ovr:75,potential:76},{name:'Nikola Pavlicevic',position:'PG',age:38,ovr:74,potential:74},{name:'Marko Pavicevic',position:'C',age:25,ovr:74,potential:77},{name:'Antonio Jordano',position:'SG',age:27,ovr:74,potential:75},{name:'Filip Andjusic',position:'C',age:24,ovr:73,potential:76},{name:'Nikola Saranovic',position:'SF',age:23,ovr:72,potential:76},{name:'Zoran Vuceljic',position:'SG',age:23,ovr:72,potential:75},{name:'Ognjen Radosic',position:'SG',age:20,ovr:71,potential:80},{name:'Matija Tomasevic',position:'PF',age:21,ovr:70,potential:78},{name:'Lazar Rakocevic',position:'PF',age:18,ovr:68,potential:81},{name:'Vuk Todorovic',position:'SG',age:18,ovr:67,potential:80},{name:'Andrija Djaletic',position:'PF',age:19,ovr:67,potential:80}
+],
+106:[
+{name:'Jovan Novak',position:'PG',age:31,ovr:77,potential:77},{name:'Anthony Roy',position:'SG',age:24,ovr:76,potential:79},{name:'Kahliel Spear',position:'PF',age:26,ovr:76,potential:77},{name:'Marko Josilo',position:'SF',age:33,ovr:75,potential:75},{name:'Marko Radovanovic',position:'PF',age:30,ovr:75,potential:75},{name:'Ranko Simovic',position:'SF',age:27,ovr:74,potential:75},{name:'Balsa Zivanovic',position:'PG',age:25,ovr:74,potential:76},{name:'Alex Steen',position:'C',age:22,ovr:73,potential:80},{name:'Mihailo Musikic',position:'C',age:24,ovr:73,potential:77},{name:'Uros Carapic',position:'SG',age:29,ovr:73,potential:73},{name:'Djordje Curcic',position:'SG',age:21,ovr:72,potential:79},{name:'Luka Roganovic',position:'PF',age:18,ovr:68,potential:82},{name:'Aleksa Obrenic',position:'PG',age:18,ovr:67,potential:81},{name:'Aleksandar Krunic',position:'SG',age:18,ovr:67,potential:81}
+]
+};
+g.BBGM_REAL_ROSTERS_MANUAL_BATCH11_202627=B;
+})(typeof globalThis!=='undefined'?globalThis:this);

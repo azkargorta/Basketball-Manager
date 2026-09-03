7db1ad700e16e450b8b8baeb0b2c150ab8db5725
azkargorta <azkardurant@gmail.com>
2026-09-03 16:50:47 +0200

    Add Estudiantes and Palencia real rosters

---
+++ js/real-rosters-manual-batch15-2026-27.js
@@ -0,0 +1,33 @@
+(function(g){'use strict';
+const B={
+37:[
+{name:'Jordan Walker',position:'SG',age:28,nationality:'USA',ovr:75,potential:75},
+{name:'Micah Speight',position:'PG',age:28,nationality:'USA',ovr:74,potential:74},
+{name:'Omar Silverio',position:'SG',age:27,nationality:'DOM',ovr:74,potential:74},
+{name:'Travis Munnings',position:'SF',age:31,nationality:'BAH',ovr:73,potential:73},
+{name:'Juampi Vaulet',position:'PF',age:29,nationality:'ARG',ovr:73,potential:73},
+{name:'Kaspar Treier',position:'PF',age:26,nationality:'EST',ovr:72,potential:74},
+{name:'Fernando Zurbriggen',position:'PG',age:29,nationality:'ARG',ovr:72,potential:72},
+{name:'Jehyve Floyd',position:'C',age:29,nationality:'USA',ovr:72,potential:72},
+{name:'Iván Aurrecoechea',position:'C',age:30,nationality:'ESP',ovr:71,potential:71},
+{name:'Petit Niang',position:'C',age:32,nationality:'SEN',ovr:71,potential:71},
+{name:'Millán Jiménez',position:'SF',age:23,nationality:'ESP',ovr:70,potential:76},
+{name:'Lucas Giovannetti',position:'SF',age:21,nationality:'ARG',ovr:69,potential:79}
+],
+39:[
+{name:'Ingus Jakovics',position:'PG',age:33,nationality:'LAT',ovr:73,potential:73},
+{name:'Spencer Littleson',position:'SG',age:28,nationality:'USA',ovr:73,potential:73},
+{name:'Josip Vrankic',position:'PF',age:27,nationality:'CAN',ovr:72,potential:73},
+{name:'Vasilije Vucetic',position:'C',age:30,nationality:'SRB',ovr:72,potential:72},
+{name:'Dani Pérez',position:'PG',age:36,nationality:'ESP',ovr:71,potential:71},
+{name:'Tobias Borg',position:'SG',age:32,nationality:'SWE',ovr:71,potential:71},
+{name:'Tanor Ngom',position:'C',age:28,nationality:'SEN',ovr:71,potential:72},
+{name:'Álvaro Muñoz',position:'SF',age:35,nationality:'ESP',ovr:70,potential:70},
+{name:'Toni Naspler',position:'PG',age:23,nationality:'ESP',ovr:69,potential:76},
+{name:'Kjell de Graaf',position:'PF',age:26,nationality:'NED',ovr:69,potential:72},
+{name:'Álex Jordá',position:'SG',age:26,nationality:'ESP',ovr:68,potential:71},
+{name:'Álvaro Martínez',position:'SF',age:23,nationality:'ESP',ovr:68,potential:75}
+]
+};
+g.BBGM_REAL_ROSTERS_MANUAL_BATCH15_202627=B;
+})(typeof globalThis!=='undefined'?globalThis:this);

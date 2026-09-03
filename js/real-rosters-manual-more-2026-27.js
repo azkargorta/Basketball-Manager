3a0b0a690d310efac509f29aee7a0000705c9701
azkargorta <azkardurant@gmail.com>
2026-09-03 13:44:08 +0200

    Add reviewed Panathinaikos ASVEL and Bayern rosters

---
+++ js/real-rosters-manual-more-2026-27.js
@@ -0,0 +1,51 @@
+(function(g){
+'use strict';
+const MORE={
+  10:[
+    {name:'Kendrick Nunn',position:'SG',age:31,nationality:'USA',ovr:88,potential:88},
+    {name:'Nigel Hayes-Davis',position:'SF',age:32,nationality:'USA',ovr:86,potential:86},
+    {name:'Cedi Osman',position:'SF',age:31,nationality:'TUR',ovr:85,potential:85},
+    {name:'Guerschon Yabusele',position:'PF',age:31,nationality:'FRA',ovr:85,potential:85},
+    {name:'Mathias Lessort',position:'C',age:31,nationality:'FRA',ovr:84,potential:84},
+    {name:'Juancho Hernangomez',position:'PF',age:31,nationality:'ESP',ovr:83,potential:83},
+    {name:'Sylvain Francisco',position:'PG',age:29,nationality:'FRA',ovr:83,potential:83},
+    {name:'Jerian Grant',position:'SG',age:34,nationality:'USA',ovr:82,potential:82},
+    {name:'Isaac Bonga',position:'SG',age:27,nationality:'GER',ovr:81,potential:82},
+    {name:'Kostas Sloukas',position:'PG',age:37,nationality:'GRE',ovr:81,potential:81},
+    {name:'Brancou Badio',position:'PG',age:28,nationality:'SEN',ovr:80,potential:80},
+    {name:'Moustapha Fall',position:'C',age:35,nationality:'FRA',ovr:79,potential:79},
+    {name:'Kostas Mitoglou',position:'PF',age:30,nationality:'GRE',ovr:79,potential:79},
+    {name:'Nikos Rogkavopoulos',position:'SF',age:25,nationality:'GRE',ovr:78,potential:81},
+    {name:'Vasilis Toliopoulos',position:'PG',age:30,nationality:'GRE',ovr:77,potential:77},
+    {name:'Alexandros Samodurov',position:'C',age:21,nationality:'GRE',ovr:72,potential:84}
+  ],
+  29:[
+    {name:'Daniel Theis',position:'C',age:34,nationality:'GER',ovr:84,potential:84},
+    {name:'Jae Crowder',position:'SF',age:36,nationality:'USA',ovr:82,potential:82},
+    {name:'Patty Mills',position:'PG',age:38,nationality:'AUS',ovr:81,potential:81},
+    {name:'Armoni Brooks',position:'SG',age:28,nationality:'USA',ovr:80,potential:80},
+    {name:'Joel Bolomboy',position:'C',age:33,nationality:'USA',ovr:80,potential:80},
+    {name:'Tamir Blatt',position:'PG',age:29,nationality:'ISR',ovr:79,potential:79},
+    {name:'Hugo Besson',position:'SG',age:25,nationality:'FRA',ovr:78,potential:81},
+    {name:'Tremont Waters',position:'PG',age:28,nationality:'PUR',ovr:78,potential:78},
+    {name:'Nate Sestina',position:'PF',age:29,nationality:'USA',ovr:77,potential:77},
+    {name:'Yves Pons',position:'SF',age:27,nationality:'FRA',ovr:77,potential:78},
+    {name:'Mathis Dossou Yovo',position:'PF',age:26,nationality:'FRA',ovr:76,potential:79},
+    {name:'Both Gach',position:'SG',age:27,nationality:'SSD',ovr:74,potential:75}
+  ],
+  35:[
+    {name:'Kamar Baldwin',position:'SG',age:29,nationality:'USA',ovr:82,potential:82},
+    {name:'Duane Washington Jr.',position:'PG',age:26,nationality:'USA',ovr:82,potential:83},
+    {name:'MarJon Beauchamp',position:'SF',age:26,nationality:'USA',ovr:81,potential:83},
+    {name:'Johannes Thiemann',position:'C',age:33,nationality:'GER',ovr:80,potential:80},
+    {name:'Austin Wiley',position:'C',age:28,nationality:'USA',ovr:79,potential:79},
+    {name:'Andreas Obst',position:'SG',age:30,nationality:'GER',ovr:79,potential:79},
+    {name:'Justus Hollatz',position:'PG',age:25,nationality:'GER',ovr:78,potential:80},
+    {name:'Miles Norris',position:'PF',age:26,nationality:'USA',ovr:77,potential:79},
+    {name:'Niels Giffey',position:'SF',age:35,nationality:'GER',ovr:76,potential:76},
+    {name:'Kilian Fischer',position:'PF',age:20,nationality:'GER',ovr:70,potential:82},
+    {name:'Niko Jerkic',position:'PG',age:19,nationality:'GER',ovr:67,potential:79}
+  ]
+};
+g.BBGM_REAL_ROSTERS_MANUAL_MORE_202627=MORE;
+})(typeof globalThis!=='undefined'?globalThis:this);

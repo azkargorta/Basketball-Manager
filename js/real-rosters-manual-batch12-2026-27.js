2863362d8a86e2e14c747dd9042018a949f0a5cc
azkargorta <azkardurant@gmail.com>
2026-09-03 16:41:03 +0200

    Add reviewed Petkim Spor 2026-27 roster

---
+++ js/real-rosters-manual-batch12-2026-27.js
@@ -0,0 +1,17 @@
+(function(g){'use strict';
+const B={
+77:[
+{name:'Keandre Cook',position:'SG',age:29,nationality:'USA',ovr:78,potential:78},
+{name:'Khyri Thomas',position:'SG',age:30,nationality:'USA',ovr:78,potential:78},
+{name:'Jarrod Uthoff',position:'PF',age:33,nationality:'USA',ovr:77,potential:77},
+{name:'Chris Horton',position:'PF',age:32,nationality:'USA',ovr:77,potential:77},
+{name:'Martynas Sajus',position:'C',age:31,nationality:'LTU',ovr:76,potential:76},
+{name:'Thomas Akyazili',position:'SG',age:30,nationality:'BEL',ovr:75,potential:75},
+{name:'Yigit Onan',position:'PF',age:24,nationality:'TUR',ovr:75,potential:79},
+{name:'Tibet Gorener',position:'C',age:24,nationality:'TUR',ovr:74,potential:79},
+{name:'Yusuf Gunduz',position:'C',age:22,nationality:'TUR',ovr:72,potential:79},
+{name:'Ozan Yilmaz',position:'SF',age:24,nationality:'TUR',ovr:71,potential:76}
+]
+};
+g.BBGM_REAL_ROSTERS_MANUAL_BATCH12_202627=B;
+})(typeof globalThis!=='undefined'?globalThis:this);

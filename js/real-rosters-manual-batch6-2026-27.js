2c4cedaedb8135588b202f29d7bc2b418721b86d
azkargorta <azkardurant@gmail.com>
2026-09-03 16:09:00 +0200

    Add five complete 2026-27 ACB rosters

---
+++ js/real-rosters-manual-batch6-2026-27.js
@@ -0,0 +1,20 @@
+(function(g){'use strict';
+const B={
+8:[
+{name:'Dylan Ennis',position:'SG',ovr:80,potential:80},{name:'Jonah Radebaugh',position:'PG',ovr:79,potential:79},{name:'Juani Marcos',position:'PG',ovr:78,potential:82},{name:'Howard Sant-Roos',position:'SF',ovr:78,potential:78},{name:'David DeJulius',position:'PG',ovr:77,potential:78},{name:'Kaiser Gates',position:'PF',ovr:77,potential:77},{name:'Marcis Steinbergs',position:'PF',ovr:76,potential:79},{name:'Jean-Marc Pansa',position:'C',ovr:76,potential:78},{name:'Souley Boum',position:'SG',ovr:75,potential:78},{name:'Sander Raieste',position:'SF',ovr:75,potential:77},{name:'Michael Forrest',position:'PG',ovr:74,potential:76},{name:'Emmanuel Cate',position:'C',ovr:74,potential:74},{name:'Moussa Diagne',position:'C',ovr:73,potential:73},{name:'Toni Nakic',position:'SF',ovr:73,potential:73},{name:'Ruben Lopez de la Torre',position:'PF',ovr:71,potential:75},{name:'Wilhelm Falk',position:'SG',ovr:69,potential:77}
+],
+16:[
+{name:'Ferran Bassas',position:'PG',ovr:77,potential:77},{name:'Timmy Allen',position:'PF',ovr:77,potential:78},{name:'Lukasz Kolenda',position:'SG',ovr:76,potential:77},{name:'Lucas Beaufort',position:'PG',ovr:75,potential:78},{name:'Chibuzo Agbo',position:'SF',ovr:75,potential:79},{name:'Pierre Oriola',position:'C',ovr:75,potential:75},{name:'Yordan Minchev',position:'PF',ovr:74,potential:75},{name:'Rafa Villar',position:'PG',ovr:73,potential:78},{name:'Eric Vila',position:'PF',ovr:73,potential:74},{name:'Nick Ongenda',position:'C',ovr:73,potential:77},{name:'Hugo Benitez',position:'PG',ovr:71,potential:77},{name:'Pablo Tamba',position:'SF',ovr:70,potential:80}
+],
+18:[
+{name:'Alonzo Verge Jr.',position:'PG',ovr:78,potential:78},{name:'Tomas Dimsa',position:'SG',ovr:77,potential:77},{name:'George Conditt IV',position:'C',ovr:77,potential:78},{name:'Dmytro Skapintsev',position:'C',ovr:76,potential:78},{name:'Lukas Uleckas',position:'SF',ovr:76,potential:76},{name:'Karlis Silins',position:'C',ovr:75,potential:75},{name:'Dino Radoncic',position:'PF',ovr:75,potential:75},{name:'Paul Jorgensen',position:'SG',ovr:74,potential:74},{name:'Guillem Jou',position:'SF',ovr:74,potential:74},{name:'Caio Pacheco',position:'PG',ovr:73,potential:74},{name:'Jacobo Diaz',position:'SF',ovr:72,potential:72},{name:'Didac Cuevas',position:'PG',ovr:71,potential:74}
+],
+19:[
+{name:'Leo Meindl',position:'SF',ovr:78,potential:78},{name:'Caleb Agada',position:'SG',ovr:77,potential:77},{name:'Brandon Childress',position:'PG',ovr:77,potential:77},{name:'Joel Soriano',position:'C',ovr:76,potential:79},{name:'Alex Barcello',position:'SG',ovr:76,potential:76},{name:'Efe Abogidi',position:'PF',ovr:75,potential:80},{name:'Felipe dos Anjos',position:'C',ovr:75,potential:75},{name:'Yunio Barrueta',position:'SF',ovr:74,potential:74},{name:'Diogo Brito',position:'SG',ovr:73,potential:74},{name:'Sergi Quintela',position:'SG',ovr:73,potential:73},{name:'Olle Lundqvist',position:'SG',ovr:72,potential:73},{name:'Alejandro Galan',position:'PF',ovr:72,potential:72},{name:'Alonso Faure',position:'PF',ovr:71,potential:75}
+],
+22:[
+{name:'Rasir Bolton',position:'PG',ovr:78,potential:79},{name:'Tevin Brown',position:'SF',ovr:77,potential:78},{name:'Danko Brankovic',position:'C',ovr:77,potential:79},{name:'Dominik Mavra',position:'PG',ovr:76,potential:76},{name:'Aleksandar Aranitovic',position:'SF',ovr:76,potential:76},{name:'Francis Alonso',position:'SG',ovr:75,potential:75},{name:'Mihajlo Andric',position:'PF',ovr:75,potential:75},{name:'Andersson Garcia',position:'PF',ovr:74,potential:80},{name:'Jonathan Kasibabu',position:'C',ovr:74,potential:74},{name:'Aleksa Ilic',position:'C',ovr:73,potential:75},{name:'Edgar Vicedo',position:'PF',ovr:73,potential:73},{name:'Erik Quintela',position:'PG',ovr:72,potential:72},{name:'Josep Peris',position:'SG',ovr:70,potential:74}
+]
+};
+g.BBGM_REAL_ROSTERS_MANUAL_BATCH6_202627=B;
+})(typeof globalThis!=='undefined'?globalThis:this);

0ecda5a229e4e4ba441a9bf9112e6a7bf92a1b6d
azkargorta <azkardurant@gmail.com>
2026-09-03 08:21:05 +0200

    Add verified missing 2026-27 rosters

---
+++ js/real-rosters-manual-extra-2026-27.js
@@ -0,0 +1,20 @@
+(function(g){'use strict';
+const EXTRA={
+11:[
+{name:'Shane Larkin',position:'PG',age:34,ovr:87,potential:87},{name:'Wade Baldwin IV',position:'SG',age:30,ovr:85,potential:85},{name:'Shavon Shields',position:'SF',age:32,ovr:84,potential:84},{name:'Talen Horton-Tucker',position:'SF',age:25,ovr:83,potential:84},{name:'Nicolo Melli',position:'PF',age:35,ovr:82,potential:82},{name:'Will Clyburn',position:'SF',age:36,ovr:81,potential:81},{name:'Tarik Biberovic',position:'SG',age:26,ovr:80,potential:82},{name:'Trent Forrest',position:'SG',age:28,ovr:79,potential:80},{name:'Sertac Sanli',position:'C',age:35,ovr:78,potential:78},{name:'Onuralp Bitim',position:'SG',age:27,ovr:78,potential:79},{name:'Braxton Key',position:'SF',age:29,ovr:77,potential:78},{name:'Marcus Bingham Jr.',position:'PF',age:26,ovr:76,potential:79},{name:'Ignas Sargiunas',position:'SG',age:27,ovr:75,potential:76},{name:'Georges Choueiry',position:'PG',age:31,ovr:72,potential:72}
+],
+27:[
+{name:'Carlik Jones',position:'PG',age:29,ovr:84,potential:84},{name:'Luca Vildoza',position:'PG',age:31,ovr:82,potential:82},{name:'Lamar Stevens',position:'SF',age:29,ovr:81,potential:81},{name:'Alessandro Pajola',position:'PG',age:27,ovr:80,potential:81},{name:'Kyle Allman Jr.',position:'SG',age:29,ovr:79,potential:79},{name:'Tonye Jekiri',position:'C',age:32,ovr:78,potential:78},{name:'Derek Willis',position:'PF',age:31,ovr:78,potential:78},{name:'Kevarrius Hayes',position:'C',age:29,ovr:77,potential:78},{name:'Nikola Tanaskovic',position:'PF',age:29,ovr:76,potential:77}
+],
+30:[
+{name:'Daniel Theis',position:'C',age:34,ovr:84,potential:84},{name:'Oshae Brissett',position:'PF',age:28,ovr:82,potential:82},{name:'Yam Madar',position:'PG',age:26,ovr:81,potential:83},{name:'Bonzie Colson Jr.',position:'PF',age:31,ovr:80,potential:80},{name:'Armando Bacot',position:'PF',age:26,ovr:79,potential:82},{name:'Gabriel Lundberg',position:'SG',age:32,ovr:79,potential:79},{name:'Keaton Wallace',position:'SG',age:28,ovr:78,potential:79},{name:'Jimmy Clark III',position:'PG',age:25,ovr:76,potential:79},{name:'Will Rayman',position:'SF',age:29,ovr:75,potential:76},{name:'John DiBartolomeo',position:'PG',age:35,ovr:74,potential:74}
+],
+31:[
+{name:'Johnathan Motley',position:'PF',age:31,ovr:82,potential:82},{name:'Jared Butler',position:'PG',age:26,ovr:81,potential:83},{name:'Patrick Baldwin Jr.',position:'SG',age:24,ovr:79,potential:83},{name:'David Kramer',position:'SG',age:30,ovr:78,potential:78},{name:'Dejan Davidovac',position:'SF',age:32,ovr:78,potential:78},{name:'Ebuka Izundu',position:'PF',age:30,ovr:77,potential:77},{name:'Vojin Medarevic',position:'C',age:24,ovr:74,potential:77},{name:'Aleksandar Krunic',position:'SG',age:18,ovr:67,potential:84}
+],
+33:[
+{name:'Jonas Valanciunas',position:'C',age:34,ovr:86,potential:86},{name:'Carsen Edwards',position:'PG',age:28,ovr:84,potential:84},{name:'Saben Lee',position:'PG',age:27,ovr:83,potential:83},{name:'Sterling Brown',position:'SG',age:32,ovr:81,potential:81},{name:'Marius Grigonis',position:'SF',age:32,ovr:80,potential:80},{name:'Maodo Lo',position:'PG',age:33,ovr:79,potential:79},{name:'Maxwell Lewis',position:'SF',age:24,ovr:78,potential:82},{name:'Azuolas Tubelis',position:'PF',age:24,ovr:78,potential:82},{name:'Dustin Sleva',position:'PF',age:31,ovr:77,potential:77},{name:'Marek Blazevic',position:'C',age:25,ovr:76,potential:80},{name:'Souleymane Berthe',position:'SF',age:26,ovr:73,potential:76}
+]
+};
+g.BBGM_REAL_ROSTERS_MANUAL_202627=EXTRA;
+})(typeof globalThis!=='undefined'?globalThis:this);

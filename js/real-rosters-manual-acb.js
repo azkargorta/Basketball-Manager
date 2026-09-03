d68e34daf8681f2987fe39c2e6f5bbd7a257060e
azkargorta <azkardurant@gmail.com>
2026-09-03 06:44:36 +0200

    Add manually validated ACB real rosters

---
+++ js/real-rosters-manual-acb.js
@@ -0,0 +1,112 @@
+(function(g){
+'use strict';
+const M=g.BBGM_REAL_ROSTERS_MANUAL_ACB_202627={
+  5:[
+    {name:'Willy Hernangomez',position:'C',age:32,nationality:'ESP',ovr:83},
+    {name:'Ignas Brazdeikis',position:'SF',age:28,nationality:'LTU',ovr:82},
+    {name:'Alberto Diaz',position:'PG',age:32,nationality:'ESP',ovr:80},
+    {name:'Amine Noua',position:'PF',age:30,nationality:'FRA',ovr:78},
+    {name:'Simas Lukosius',position:'SG',age:24,nationality:'LTU',ovr:77,potential:82},
+    {name:'Jonathan Barreiro',position:'SF',age:30,nationality:'ESP',ovr:77},
+    {name:'Nihad Djedovic',position:'SG',age:37,nationality:'BIH',ovr:75},
+    {name:'Yannick Nzosa',position:'C',age:23,nationality:'COD',ovr:73,potential:82},
+    {name:'Erol Can Cinko',position:'PG',age:36,nationality:'TUR',ovr:70}
+  ],
+  6:[
+    {name:'Nicolas Laprovittola',position:'PG',age:37,nationality:'ARG',ovr:83},
+    {name:'Ricky Rubio',position:'PG',age:35,nationality:'ESP',ovr:82},
+    {name:'Ante Tomic',position:'C',age:39,nationality:'CRO',ovr:81},
+    {name:'Ludde Hakanson',position:'PG',age:30,nationality:'SWE',ovr:78},
+    {name:'Simon Birgander',position:'C',age:29,nationality:'SWE',ovr:78},
+    {name:'Emir Sulejmanovic',position:'PF',age:31,nationality:'BIH',ovr:76},
+    {name:'Boogie Ellis',position:'PG',age:26,nationality:'USA',ovr:76,potential:79},
+    {name:'Yannick Kraag',position:'SF',age:24,nationality:'NED',ovr:75,potential:80},
+    {name:'Kwan Cheatham Jr.',position:'PF',age:31,nationality:'USA',ovr:75},
+    {name:'Alex Reyes',position:'SF',age:33,nationality:'ESP',ovr:73},
+    {name:'Isaac Nogues',position:'SG',age:23,nationality:'ESP',ovr:72,potential:80},
+    {name:'Miguel Allen',position:'C',age:23,nationality:'ESP',ovr:70,potential:78}
+  ],
+  13:[
+    {name:'Jilson Bango',position:'C',ovr:80},
+    {name:'Nicolas Brussino',position:'SF',nationality:'ARG',ovr:80},
+    {name:'Caleb Homesley',position:'SG',nationality:'USA',ovr:79},
+    {name:'Laurynas Birutis',position:'C',nationality:'LTU',ovr:78},
+    {name:'Trae Bell-Haynes',position:'PG',nationality:'CAN',ovr:78},
+    {name:'Justin Jaworski',position:'SG',nationality:'USA',ovr:76},
+    {name:'Guillem Vives',position:'PG',nationality:'ESP',ovr:75},
+    {name:'Roberts Blumbergs',position:'PF',nationality:'LAT',ovr:75},
+    {name:'Sergi Garcia',position:'PG',nationality:'ESP',ovr:74},
+    {name:'Miguel Gonzalez',position:'SF',nationality:'ESP',ovr:72},
+    {name:'Jaime Fernandez',position:'C',nationality:'ESP',ovr:71}
+  ],
+  14:[
+    {name:'Mac McClung',position:'PG',age:27,nationality:'USA',ovr:80},
+    {name:'Martin Geben',position:'C',nationality:'LTU',ovr:76},
+    {name:'Aljaz Kunc',position:'PF',nationality:'SLO',ovr:75},
+    {name:'Nikola Maric',position:'PF',nationality:'BIH',ovr:75},
+    {name:'Sergi Martinez',position:'SF',nationality:'ESP',ovr:74},
+    {name:'Maximo Fjellerup',position:'SG',nationality:'ARG',ovr:74},
+    {name:'David N\'Guessan',position:'C',nationality:'NED',ovr:73},
+    {name:'Andrzej Pluta',position:'SG',nationality:'POL',ovr:73},
+    {name:'Pep Busquets',position:'SF',nationality:'ESP',ovr:72},
+    {name:'Stanley Whittaker',position:'PG',nationality:'USA',ovr:72},
+    {name:'Isaac Vazquez',position:'SG',nationality:'ESP',ovr:69,potential:75}
+  ],
+  15:[
+    {name:'John Shurna',position:'PF',nationality:'USA',ovr:79},
+    {name:'Melvin Ejim',position:'SF',nationality:'CAN',ovr:78},
+    {name:'Darius McGhee',position:'PG',nationality:'USA',ovr:77},
+    {name:'James Batemon III',position:'PG',nationality:'USA',ovr:76},
+    {name:'Oriol Pauli',position:'SF',nationality:'ESP',ovr:75},
+    {name:'Zacharie Perrin',position:'PF',nationality:'FRA',ovr:74,potential:80},
+    {name:'Atoumane Diagne',position:'C',nationality:'SEN',ovr:73},
+    {name:'Daniel Garcia',position:'PG',nationality:'ESP',ovr:72},
+    {name:'Mikel Sanz',position:'SF',nationality:'ESP',ovr:71},
+    {name:'Aurimas Majauskas',position:'PF',nationality:'LTU',ovr:71},
+    {name:'Adria Rodriguez',position:'PG',nationality:'ESP',ovr:68,potential:75}
+  ],
+  17:[
+    {name:'Marcelinho Huertas',position:'PG',nationality:'BRA',ovr:84},
+    {name:'Giorgi Shermadini',position:'C',nationality:'GEO',ovr:82},
+    {name:'Kyle Guy',position:'SG',nationality:'USA',ovr:81},
+    {name:'Rokas Giedraitis',position:'SF',nationality:'LTU',ovr:80},
+    {name:'Ethan Happ',position:'C',nationality:'USA',ovr:79},
+    {name:'Bruno Fitipaldo',position:'PG',nationality:'URU',ovr:78},
+    {name:'Fran Guerra',position:'C',nationality:'ESP',ovr:77},
+    {name:'Jaime Fernandez',position:'PG',nationality:'ESP',ovr:77},
+    {name:'Xabier Lopez-Arostegui',position:'SF',nationality:'ESP',ovr:77},
+    {name:'T.J. Bamba',position:'SF',nationality:'USA',ovr:76},
+    {name:'Tim Abromaitis',position:'PF',nationality:'USA',ovr:75},
+    {name:'Aaron Doornekamp',position:'PF',nationality:'CAN',ovr:74},
+    {name:'Wesley Van Beck',position:'SG',nationality:'USA',ovr:74},
+    {name:'Hector Alderete',position:'SF',nationality:'ESP',ovr:71,potential:77}
+  ],
+  21:[
+    {name:'Vlatko Cancar',position:'SF',nationality:'SLO',ovr:82},
+    {name:'Retin Obasohan',position:'PG',nationality:'BEL',ovr:78},
+    {name:'Ziga Samar',position:'PG',nationality:'SLO',ovr:77},
+    {name:'Balsa Koprivica',position:'C',nationality:'SRB',ovr:77},
+    {name:'D.J. Steward',position:'PG',nationality:'USA',ovr:76},
+    {name:'Christian Sengfelder',position:'PF',nationality:'GER',ovr:76},
+    {name:'Chase Audige',position:'SG',nationality:'USA',ovr:75},
+    {name:'Pablo Almazan',position:'SF',nationality:'ESP',ovr:74},
+    {name:'Dusan Radosavljevic',position:'SF',nationality:'SRB',ovr:71},
+    {name:'Inaki Ordonez',position:'SF',nationality:'ESP',ovr:69,potential:75}
+  ],
+  23:[
+    {name:'Darrun Hilliard',position:'SG',nationality:'USA',ovr:79},
+    {name:'Harald Frey',position:'PG',nationality:'NOR',ovr:78},
+    {name:'Tryggvi Hlinason',position:'C',nationality:'ISL',ovr:78},
+    {name:'Martin Krampelj',position:'PF',nationality:'SLO',ovr:76},
+    {name:'Luke Petrasek',position:'PF',nationality:'USA',ovr:76},
+    {name:'Margiris Normantas',position:'SG',nationality:'LTU',ovr:75},
+    {name:'Nate Darling',position:'SG',nationality:'CAN',ovr:75},
+    {name:'Bastien Vautier',position:'C',nationality:'FRA',ovr:74},
+    {name:'Adam Somogyi',position:'SG',nationality:'HUN',ovr:73},
+    {name:'Dan Duscak',position:'PG',nationality:'SLO',ovr:71},
+    {name:'Aleix Font',position:'SG',nationality:'ESP',ovr:70},
+    {name:'Ondrej Hustak',position:'C',nationality:'CZE',ovr:69,potential:74},
+    {name:'Manex Ansorregui',position:'SF',nationality:'ESP',ovr:68,potential:74}
+  ]
+};
+})(typeof globalThis!=='undefined'?globalThis:this);

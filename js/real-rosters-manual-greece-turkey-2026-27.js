ca71f27f5ae88f3335e4ad808f067526cdc557d1
azkargorta <azkardurant@gmail.com>
2026-09-03 14:43:36 +0200

    Add verified Greece and Turkey roster batch

---
+++ js/real-rosters-manual-greece-turkey-2026-27.js
@@ -0,0 +1,55 @@
+(function(g){
+'use strict';
+g.BBGM_REAL_ROSTERS_MANUAL_GREECE_TURKEY_202627={
+32:[
+{name:'Scottie Wilbekin',position:'PG',age:33,nationality:'TUR',ovr:83,potential:83},
+{name:'Furkan Korkmaz',position:'SG',age:29,nationality:'TUR',ovr:82,potential:82},
+{name:'Ante Zizic',position:'C',age:30,nationality:'CRO',ovr:81,potential:81},
+{name:'Jaylen Nowell',position:'SG',age:27,nationality:'USA',ovr:80,potential:80},
+{name:'Wenyen Gabriel',position:'PF',age:29,nationality:'SSD',ovr:79,potential:79},
+{name:'DaQuan Jeffries',position:'SF',age:29,nationality:'USA',ovr:78,potential:78},
+{name:'Devon Dotson',position:'PG',age:27,nationality:'USA',ovr:78,potential:78},
+{name:'Eugene Omoruyi',position:'SF',age:30,nationality:'CAN',ovr:77,potential:77},
+{name:'Conor Morgan',position:'PF',age:32,nationality:'CAN',ovr:76,potential:76},
+{name:'Mady Sissoko',position:'C',age:26,nationality:'USA',ovr:76,potential:78},
+{name:'David DeJulius',position:'PG',age:27,nationality:'USA',ovr:75,potential:76},
+{name:'James Birsen',position:'SF',age:31,nationality:'TUR',ovr:74,potential:74}
+],
+74:[
+{name:'Bryce Jones',position:'PG',age:32,nationality:'USA',ovr:78,potential:78},
+{name:"Tre'Shawn Thurman",position:'SF',age:30,nationality:'USA',ovr:77,potential:77},
+{name:'Gabe Brown',position:'SF',age:26,nationality:'USA',ovr:76,potential:77},
+{name:'Terrell Carter II',position:'C',age:31,nationality:'USA',ovr:76,potential:76},
+{name:'Sadik Kabaca',position:'C',age:26,nationality:'TUR',ovr:75,potential:77},
+{name:'Jamuni McNeace',position:'C',age:30,nationality:'USA',ovr:75,potential:75},
+{name:'Zach Nutall',position:'PG',age:27,nationality:'USA',ovr:74,potential:75},
+{name:'Shavar Reynolds Jr.',position:'SG',age:28,nationality:'USA',ovr:74,potential:74},
+{name:'Leon Harun Apaydin',position:'SG',age:28,nationality:'TUR',ovr:72,potential:72},
+{name:'Karahan Efeoglu',position:'SF',age:22,nationality:'TUR',ovr:70,potential:79}
+],
+80:[
+{name:'Nenad Dimitrijevic',position:'PG',age:29,nationality:'MKD',ovr:82,potential:82},
+{name:'Jeremiah Robinson-Earl',position:'PF',age:25,nationality:'USA',ovr:81,potential:83},
+{name:'E.J. Liddell',position:'PF',age:26,nationality:'USA',ovr:80,potential:81},
+{name:'Khem Birch',position:'C',age:34,nationality:'CAN',ovr:79,potential:79},
+{name:'Kostas Antetokounmpo',position:'PF',age:29,nationality:'GRE',ovr:79,potential:79},
+{name:'Matt Morgan',position:'SG',age:29,nationality:'USA',ovr:78,potential:78},
+{name:'Adam Mokoka',position:'SG',age:28,nationality:'FRA',ovr:77,potential:77},
+{name:'Vasilis Charalambopoulos',position:'SF',age:30,nationality:'GRE',ovr:76,potential:76},
+{name:'Vasilis Toliopoulos',position:'PG',age:30,nationality:'GRE',ovr:76,potential:76},
+{name:'Thanasis Antetokounmpo',position:'SF',age:34,nationality:'GRE',ovr:75,potential:75},
+{name:'Georgios Tanoulis',position:'C',age:24,nationality:'GRE',ovr:73,potential:79}
+],
+81:[
+{name:'Cedi Osman',position:'SF',age:31,nationality:'TUR',ovr:84,potential:84},
+{name:'Nick Calathes',position:'PG',age:38,nationality:'GRE',ovr:81,potential:81},
+{name:'Marcus Foster',position:'SG',age:31,nationality:'USA',ovr:80,potential:80},
+{name:'Raiquan Gray',position:'PF',age:27,nationality:'USA',ovr:79,potential:79},
+{name:'Kyle Alexander',position:'C',age:30,nationality:'CAN',ovr:78,potential:78},
+{name:'Naz Long',position:'SG',age:33,nationality:'CAN',ovr:77,potential:77},
+{name:'Trevor Hudgins',position:'PG',age:27,nationality:'USA',ovr:76,potential:77},
+{name:'Nikos Chougkaz',position:'PF',age:26,nationality:'GRE',ovr:75,potential:77},
+{name:'Nasos Bazinas',position:'PG',age:23,nationality:'GRE',ovr:72,potential:79}
+]
+};
+})(typeof globalThis!=='undefined'?globalThis:this);

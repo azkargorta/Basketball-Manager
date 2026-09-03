6eaac59584e0150e14ca7799bbe8d7a0988ea651
azkargorta <azkardurant@gmail.com>
2026-09-03 06:48:29 +0200

    Add validated Italian real rosters

---
+++ js/real-rosters-manual-italy.js
@@ -0,0 +1,73 @@
+(function(g){
+'use strict';
+g.BBGM_REAL_ROSTERS_MANUAL_ITALY_202627={
+  54:[
+    {name:'Kyle Wiltjer',position:'PF',nationality:'CAN',ovr:80},
+    {name:'Louis Olinde',position:'SF',nationality:'GER',ovr:79},
+    {name:'Enoch Boakye',position:'C',nationality:'CAN',ovr:78,potential:82},
+    {name:'Ky Bowman',position:'SG',nationality:'USA',ovr:78},
+    {name:'Glynn Watson',position:'PG',nationality:'USA',ovr:77},
+    {name:"Sir'Jabari Rice",position:'SG',nationality:'USA',ovr:76},
+    {name:'Amedeo Tessitori',position:'C',nationality:'ITA',ovr:76},
+    {name:'Leonardo Candi',position:'PG',nationality:'ITA',ovr:74},
+    {name:'Sasha Grant',position:'SF',nationality:'ITA',ovr:73},
+    {name:'Giga Janelidze',position:'PF',nationality:'GEO',ovr:72},
+    {name:'Octavio Maretto',position:'SF',nationality:'ARG',ovr:70,potential:77}
+  ],
+  55:[
+    {name:'Quincy Olivari',position:'SG',nationality:'USA',age:25,ovr:79},
+    {name:'Charlie Brown Jr.',position:'SG',nationality:'USA',age:30,ovr:77},
+    {name:'Darius Brown II',position:'PG',nationality:'USA',age:27,ovr:76},
+    {name:'Selom Mawugbe',position:'C',nationality:'USA',age:28,ovr:76},
+    {name:'Jordan Philippe Bayehe',position:'PF',nationality:'CMR',age:27,ovr:75},
+    {name:'Isaiah Bigelow',position:'SF',nationality:'USA',age:27,ovr:74},
+    {name:'Toto Forray',position:'PG',nationality:'ARG',age:40,ovr:72},
+    {name:'Theo Airhienbuwa',position:'SF',nationality:'ITA',age:20,ovr:70,potential:79},
+    {name:'Patrick Hassan',position:'PG',nationality:'ITA',age:20,ovr:68,potential:76},
+    {name:'Cheickh Niang',position:'SF',nationality:'ITA',age:18,ovr:66,potential:80},
+    {name:'Federico Cattapan',position:'SF',nationality:'ITA',age:18,ovr:65,potential:77}
+  ],
+  56:[
+    {name:'Prentiss Hubb',position:'PG',nationality:'USA',age:27,ovr:79},
+    {name:'Justin Gorham',position:'PF',nationality:'USA',age:28,ovr:78},
+    {name:'Dominik Olejniczak',position:'C',nationality:'POL',age:30,ovr:77},
+    {name:'Giordano Bortolani',position:'SG',nationality:'ITA',age:26,ovr:77},
+    {name:'A.J. Durham',position:'SG',nationality:'USA',age:28,ovr:76},
+    {name:'Karim Jallow',position:'SF',nationality:'GER',age:29,ovr:76},
+    {name:'Dante Maddox Jr.',position:'SG',nationality:'USA',age:25,ovr:75},
+    {name:'Andrea Pecchia',position:'SF',nationality:'ITA',age:29,ovr:74},
+    {name:'Paul Eboua',position:'PF',nationality:'CMR',age:27,ovr:73},
+    {name:'Giulio Gazzotti',position:'PF',nationality:'ITA',age:35,ovr:71},
+    {name:'Edoardo Di Meo',position:'PG',nationality:'ITA',age:18,ovr:64,potential:77}
+  ],
+  57:[
+    {name:'J.T. Thor',position:'PF',nationality:'SSD',ovr:81},
+    {name:'Speedy Smith',position:'PG',nationality:'USA',ovr:79},
+    {name:'Jaylen Barford',position:'SG',nationality:'USA',ovr:78},
+    {name:'Derek Ogbeide',position:'C',nationality:'NGA',ovr:77},
+    {name:'Jaime Echenique',position:'C',nationality:'COL',ovr:77},
+    {name:'Rob Edwards',position:'SG',nationality:'USA',ovr:76},
+    {name:'Riccardo Rossato',position:'PG',nationality:'ITA',ovr:75},
+    {name:'Luca Severini',position:'C',nationality:'ITA',ovr:74},
+    {name:'Tomas Woldetensae',position:'SG',nationality:'ITA',ovr:74},
+    {name:'Lorenzo Uglietti',position:'SG',nationality:'ITA',ovr:73},
+    {name:'Riccardo Visconti',position:'SG',nationality:'ITA',ovr:73},
+    {name:'Elhadji Fainke',position:'PF',nationality:'MLI',ovr:69,potential:78}
+  ],
+  60:[
+    {name:'Amedeo Della Valle',position:'SG',nationality:'ITA',age:33,ovr:80},
+    {name:'Isaiah Roby',position:'SF',nationality:'USA',age:29,ovr:80},
+    {name:'Ismael Kamagate',position:'C',nationality:'FRA',age:26,ovr:79},
+    {name:'David Duke',position:'SG',nationality:'USA',age:27,ovr:78},
+    {name:'Hunter Hale',position:'SG',nationality:'USA',age:29,ovr:78},
+    {name:'William McDowell-White',position:'PG',nationality:'AUS',age:28,ovr:77},
+    {name:'Nate Renfro',position:'SF',nationality:'USA',age:30,ovr:76},
+    {name:'Davide Alviti',position:'SF',nationality:'ITA',age:30,ovr:76},
+    {name:'Matteo Librizzi',position:'PG',nationality:'ITA',age:24,ovr:74,potential:78},
+    {name:'Maximilian Ladurner',position:'C',nationality:'ITA',age:25,ovr:73},
+    {name:'Stefan Nikolic',position:'SG',nationality:'SRB',age:29,ovr:72},
+    {name:'Nongo Ikangi',position:'PF',nationality:'ITA',age:33,ovr:70},
+    {name:'Mauro Villa',position:'SF',nationality:'ARG',age:22,ovr:68,potential:76}
+  ]
+};
+})(typeof globalThis!=='undefined'?globalThis:this);

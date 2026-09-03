33de3ea07c389454a5f3cf598bfb9a4e3742dc95
azkargorta <azkardurant@gmail.com>
2026-09-03 15:54:04 +0200

    Add Lithuania Israel and Adriatic roster batch

---
+++ js/real-rosters-manual-batch4-2026-27.js
@@ -0,0 +1,26 @@
+(function(g){'use strict';
+const B={
+40:[
+{name:'Wes Dreamer',position:'PF',age:26,ovr:70,potential:72},{name:'Sediq Garuba',position:'SG',age:23,ovr:69,potential:76},{name:'George Korsantia',position:'PF',age:29,ovr:70,potential:70},{name:'Rafael Lisboa',position:'PG',age:27,ovr:71,potential:72},{name:'Daniel Manchon',position:'PG',age:28,ovr:69,potential:69},{name:'Mikhail Mikhailov',position:'C',age:24,ovr:68,potential:72},{name:'Kristinn Palsson',position:'SF',age:29,ovr:71,potential:71},{name:'Arnau Parrado',position:'C',age:26,ovr:70,potential:72}
+],
+42:[
+{name:'Lucas Capalbo',position:'PG',age:26,ovr:71,potential:72},{name:'Adria Domenech',position:'C',age:24,ovr:69,potential:73},{name:'Corentin Falcoz',position:'SF',age:24,ovr:69,potential:72},{name:'Ondrej Hanzlik',position:'SF',age:24,ovr:71,potential:73},{name:'Omar Lo',position:'PF',age:29,ovr:72,potential:72},{name:'Gaizka Maiza',position:'PG',age:30,ovr:70,potential:70},{name:'Jonas Paukste',position:'C',age:26,ovr:71,potential:72},{name:'Brian Vazquez Arraiza',position:'SG',age:32,ovr:70,potential:70}
+],
+85:[
+{name:'Yogi Ferrell',position:'PG',age:33,ovr:81,potential:81},{name:'Marial Shayok',position:'SG',age:31,ovr:80,potential:80},{name:'Axel Bouteille',position:'SF',age:31,ovr:79,potential:79},{name:'Juwan Morgan',position:'PF',age:29,ovr:79,potential:79},{name:'Justin Smith',position:'SF',age:28,ovr:78,potential:79},{name:'Iverson Molinar',position:'SG',age:27,ovr:78,potential:79},{name:'Jerry Boutsiele',position:'C',age:34,ovr:78,potential:78},{name:'Fletcher Magee',position:'SG',age:30,ovr:77,potential:77},{name:'Alexander Kovlyar',position:'PG',age:24,ovr:77,potential:80},{name:'Stefan Djordjevic',position:'PF',age:28,ovr:76,potential:77},{name:'Djordije Jovanovic',position:'SG',age:23,ovr:75,potential:79},{name:'Emir Hadzibegovic',position:'PF',age:30,ovr:74,potential:74}
+],
+97:[
+{name:'Arturas Gudaitis',position:'C',age:33,ovr:79,potential:79},{name:'Michael Devoe',position:'SG',age:27,ovr:78,potential:79},{name:'Gytis Masiulis',position:'PF',age:28,ovr:77,potential:77},{name:'Gytis Radzevicius',position:'SF',age:31,ovr:77,potential:77},{name:'Matas Jogela',position:'SF',age:28,ovr:76,potential:77},{name:'Kay Bruhnke',position:'SG',age:25,ovr:76,potential:79},{name:'Ricky Lindo',position:'PF',age:26,ovr:75,potential:77},{name:'Oskaras Pleikys',position:'C',age:25,ovr:74,potential:78},{name:'Nino Dim',position:'SF',age:26,ovr:73,potential:75}
+],
+101:[
+{name:'Mike Caffey',position:'PG',age:34,ovr:76,potential:76},{name:'Cedric Henderson Jr.',position:'SF',age:26,ovr:75,potential:77},{name:'Tauras Jogela',position:'SF',age:33,ovr:74,potential:74},{name:'Karolis Lukosiunas',position:'SF',age:29,ovr:74,potential:74},{name:'Simas Jarumbauskas',position:'SG',age:26,ovr:73,potential:75},{name:'Martynas Pacevicius',position:'C',age:29,ovr:73,potential:73},{name:'Adrees Zeew',position:'SF',age:26,ovr:72,potential:74},{name:'Francesco Deri',position:'SF',age:21,ovr:69,potential:78}
+],
+102:[
+{name:'Jared Harper',position:'PG',age:29,ovr:82,potential:82},{name:'Shake Milton',position:'SG',age:30,ovr:81,potential:81},{name:'Kenny Lofton Jr.',position:'PF',age:24,ovr:79,potential:82},{name:'Devontae Cacok',position:'PF',age:30,ovr:78,potential:78},{name:'Dusan Miletic',position:'C',age:28,ovr:77,potential:78},{name:'Yotam Hanochi',position:'SF',age:26,ovr:76,potential:78},{name:'Gabriel Chachashvili',position:'C',age:27,ovr:75,potential:76},{name:'Nimrod Levi',position:'PF',age:31,ovr:75,potential:75},{name:'Josiah James',position:'SF',age:26,ovr:74,potential:76},{name:'Ethan Burg',position:'SG',age:24,ovr:73,potential:77}
+],
+105:[
+{name:'Tyreek Smith',position:'PF',age:27,ovr:74,potential:76},{name:'Gabe Wiznitzer',position:'C',age:25,ovr:73,potential:76},{name:'Golan Gutt',position:'SG',age:32,ovr:72,potential:72},{name:'Noam Avivi',position:'SF',age:28,ovr:71,potential:72},{name:'Ido Flaisher',position:'C',age:29,ovr:71,potential:71},{name:'Shachar Doron',position:'SG',age:21,ovr:69,potential:77},{name:'Ofri Shitrit',position:'SF',age:21,ovr:68,potential:76},{name:'Ido Kkanterovich',position:'PG',age:19,ovr:66,potential:77},{name:'Matan Fogel',position:'SF',age:19,ovr:65,potential:76}
+]
+};
+g.BBGM_REAL_ROSTERS_MANUAL_BATCH4_202627=B;
+})(typeof globalThis!=='undefined'?globalThis:this);

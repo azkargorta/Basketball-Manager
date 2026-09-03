459f8ab5911afbc94f3cb295a86fbac4930adc74
azkargorta <azkardurant@gmail.com>
2026-09-03 16:25:56 +0200

    Add Mersin and Cluj real rosters

---
+++ js/real-rosters-manual-batch8-2026-27.js
@@ -0,0 +1,11 @@
+(function(g){'use strict';
+const B={
+76:[
+{name:'Kruize Pinkins',position:'PF',age:33,ovr:77,potential:77},{name:'Orhan Haciyeva',position:'SF',age:37,ovr:75,potential:75},{name:'Dorukhan Engindeniz',position:'SG',age:34,ovr:74,potential:74},{name:'Mustafa Kurtuldum',position:'SG',age:25,ovr:74,potential:77},{name:'Yesukan Onar',position:'PG',age:24,ovr:73,potential:78},{name:'Yigit Baran Sarikaya',position:'PF',age:27,ovr:72,potential:74},{name:'Alp Karahan',position:'SG',age:28,ovr:72,potential:73},{name:'Arca Tuluoglu',position:'PG',age:29,ovr:71,potential:72}
+],
+95:[
+{name:'Malcolm Hill',position:'SF',age:31,ovr:81,potential:81},{name:'Javon Bess',position:'SF',age:30,ovr:79,potential:79},{name:'Dusan Beslac',position:'PF',age:28,ovr:78,potential:78},{name:'Jalyn McCreary',position:'PF',age:26,ovr:78,potential:79},{name:'Christian Bishop',position:'SF',age:26,ovr:77,potential:79},{name:'Kristian Kullamae',position:'SG',age:27,ovr:77,potential:78},{name:'Nicolae-Bogdan Nicolescu',position:'SF',age:30,ovr:75,potential:75},{name:'Luca Illes',position:'SF',age:20,ovr:71,potential:81},{name:'Radu-Andrei Cepoi',position:'PG',age:18,ovr:68,potential:82},{name:'Thomas Bernat',position:'PG',age:17,ovr:66,potential:83}
+]
+};
+g.BBGM_REAL_ROSTERS_MANUAL_BATCH8_202627=B;
+})(typeof globalThis!=='undefined'?globalThis:this);

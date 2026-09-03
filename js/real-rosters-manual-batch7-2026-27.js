a4842ac5d98e0a775ccea1d84b1d6dd74b45bdf8
azkargorta <azkardurant@gmail.com>
2026-09-03 16:17:10 +0200

    Add ALBA Wurzburg and Hamburg 2026-27 rosters

---
+++ js/real-rosters-manual-batch7-2026-27.js
@@ -0,0 +1,14 @@
+(function(g){'use strict';
+const B={
+61:[
+{name:'Joe Wieskamp',position:'SF',age:27,ovr:79,potential:79},{name:'Martin Hermannsson',position:'PG',age:32,ovr:78,potential:78},{name:'Justin Bean',position:'PF',age:30,ovr:77,potential:77},{name:'Norris Agbakoko',position:'C',age:27,ovr:77,potential:78},{name:'Brandon Angel',position:'PF',age:25,ovr:76,potential:79},{name:'Malte Delow',position:'SG',age:25,ovr:76,potential:78},{name:'Julius Boehmer',position:'PG',age:25,ovr:74,potential:77},{name:'Anton Nufer',position:'SG',age:20,ovr:71,potential:80},{name:'Richard Schmitt',position:'PF',age:18,ovr:68,potential:82},{name:'Nevio Bennefeld',position:'C',age:20,ovr:69,potential:81}
+],
+63:[
+{name:'Kenny Dye',position:'PG',age:26,ovr:77,potential:78},{name:'Charles Thompson',position:'PF',age:24,ovr:77,potential:80},{name:'Calvin Wishart',position:'SG',age:27,ovr:76,potential:77},{name:'Marvin Ogunsipe',position:'PF',age:30,ovr:75,potential:75},{name:'Alen Pjanic',position:'SF',age:29,ovr:75,potential:75},{name:'Lukas Herzog',position:'SG',age:25,ovr:74,potential:76},{name:'Brandon Tischler',position:'SG',age:26,ovr:74,potential:75},{name:'Leo Saffer',position:'C',age:26,ovr:73,potential:75},{name:'Marko Petric',position:'SG',age:19,ovr:70,potential:80},{name:'Christian Skladanowski',position:'SF',age:26,ovr:72,potential:73},{name:'Jervis Scheffs',position:'SG',age:19,ovr:68,potential:79}
+],
+68:[
+{name:'Kenneth Ogbe',position:'SG',age:32,ovr:75,potential:75},{name:'Martin Breunig',position:'PF',age:35,ovr:74,potential:74},{name:'David Muenkat',position:'SF',age:26,ovr:74,potential:76},{name:'Zsombor Maronka',position:'PF',age:24,ovr:73,potential:77},{name:'Mathys Kangudia',position:'PG',age:22,ovr:72,potential:79},{name:'Rich Igbineweka',position:'SG',age:28,ovr:72,potential:73},{name:'Emil Marshall',position:'SF',age:26,ovr:71,potential:73},{name:'Rene Kindzeka',position:'PG',age:31,ovr:70,potential:70},{name:'Darren Egbe',position:'PG',age:20,ovr:67,potential:77},{name:'Jayden Fatnassi',position:'SG',age:19,ovr:66,potential:78}
+]
+};
+g.BBGM_REAL_ROSTERS_MANUAL_BATCH7_202627=B;
+})(typeof globalThis!=='undefined'?globalThis:this);

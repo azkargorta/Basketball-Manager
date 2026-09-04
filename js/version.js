1d3a2ec6084b85773c2df15d052770c741d8303e
azkargorta <azkardurant@gmail.com>
2026-09-04 14:01:42 +0200

    Release v0.36 and refresh offline cache

--- js/version.js
+++ js/version.js
@@ -1,10 +1,10 @@
 (function(g){
 'use strict';
 const version=Object.freeze({
-  code:'0.35.0-beta',
-  label:'v0.35 Beta',
-  cacheName:'basketball-gm-beta-v035',
-  saveFormat:'basketball-manager-v035',
+  code:'0.36.0-beta',
+  label:'v0.36 Beta',
+  cacheName:'basketball-gm-beta-v036',
+  saveFormat:'basketball-manager-v036',
   dataPackVersion:'2026-09-03b'
 });
 g.BBGM_VERSION=version;

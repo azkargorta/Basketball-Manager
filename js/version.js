723b584c08a573287e6ee620be4cd8e8cc2c3a66
azkargorta <azkardurant@gmail.com>
2026-09-05 19:01:02 +0200

    Fix coherent decisive match sequence v0.48.11

--- js/version.js
+++ js/version.js
@@ -1,9 +1,9 @@
 (function(g){
 'use strict';
 const version=Object.freeze({
-  code:'0.48.10-beta',
-  label:'v0.48.10 Beta',
-  cacheName:'basketball-gm-beta-v04810',
+  code:'0.48.11-beta',
+  label:'v0.48.11 Beta',
+  cacheName:'basketball-gm-beta-v04811',
   saveFormat:'basketball-manager-v046',
   dataPackVersion:'2026-09-03b'
 });

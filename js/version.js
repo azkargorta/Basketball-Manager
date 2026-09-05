f13f5d2345546e60ea459ed1f9c9a91eb553a849
azkargorta <azkardurant@gmail.com>
2026-09-05 19:05:48 +0200

    Make season pressure narrative context aware v0.48.12

--- js/version.js
+++ js/version.js
@@ -1,9 +1,9 @@
 (function(g){
 'use strict';
 const version=Object.freeze({
-  code:'0.48.11-beta',
-  label:'v0.48.11 Beta',
-  cacheName:'basketball-gm-beta-v04811',
+  code:'0.48.12-beta',
+  label:'v0.48.12 Beta',
+  cacheName:'basketball-gm-beta-v04812',
   saveFormat:'basketball-manager-v046',
   dataPackVersion:'2026-09-03b'
 });

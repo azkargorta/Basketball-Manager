92d2216a9f0d1ddc2b6291cb466b52aedea54f1b
azkargorta <azkardurant@gmail.com>
2026-09-05 17:15:31 +0200

    Fix next match simulation after preseason v0.48.6

--- js/version.js
+++ js/version.js
@@ -1,9 +1,9 @@
 (function(g){
 'use strict';
 const version=Object.freeze({
-  code:'0.48.5-beta',
-  label:'v0.48.5 Beta',
-  cacheName:'basketball-gm-beta-v0485',
+  code:'0.48.6-beta',
+  label:'v0.48.6 Beta',
+  cacheName:'basketball-gm-beta-v0486',
   saveFormat:'basketball-manager-v046',
   dataPackVersion:'2026-09-03b'
 });
